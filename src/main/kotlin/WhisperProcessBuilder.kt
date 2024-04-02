0c0f455ec6801c697bc29da20b0bcb79c89d1c63
Giovanni Tieghi Pepi <gtpepi@proton.me>
2024-04-01 22:38:01 -0300

    Add File handler

--- src/main/kotlin/WhisperProcessBuilder.kt
+++ src/main/kotlin/WhisperProcessBuilder.kt
@@ -1,13 +1,17 @@
 import java.io.File
+import java.io.IOError
+import java.io.IOException
 
 internal class WhisperProcessBuilder {
     fun runWhisper(
-        inputFile: File,
+        inputFile: File = File("~/whisperInput/t.mp4"),
         language: String = "Portuguese",
+        outputDir: File,
         outputFormat: String = "txt",
     ): Int {
+
         val result = ProcessBuilder(
-            "whisper", "$inputFile", "--output_format", outputFormat, "--output_dir", "/home/a/Downloads",
+            "whisper", "$inputFile", "--output_format", outputFormat, "--output_dir", "$outputDir",
             "--language", language
         )
             .redirectOutput(ProcessBuilder.Redirect.INHERIT)
--- src/test/kotlin/WhisperProcessBuilderTest.kt
+++ src/test/kotlin/WhisperProcessBuilderTest.kt
@@ -5,9 +5,13 @@ import kotlin.test.Test
 
 class WhisperProcessBuilderTest {
     private val inputFile = File("/home/a/Downloads/t.mp4")
+
     @Test
     fun `Given a command, execute whisper and return the result`() {
-        val result = WhisperProcessBuilder().runWhisper(inputFile = inputFile)
+        val result = WhisperProcessBuilder().runWhisper(
+            inputFile = inputFile,
+            outputDir = File("/home/a/Downloads")
+        )
 
         result shouldBe 0
     }
