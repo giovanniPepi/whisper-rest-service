4ad2215006e1231644492327e5574dde2b9bd7d2
Giovanni Tieghi Pepi <gtpepi@proton.me>
2024-04-08 15:32:16 -0300

    Update server port config Update file handling

--- src/main/kotlin/RestHandler.kt
+++ src/main/kotlin/RestHandler.kt
@@ -3,11 +3,11 @@ package whisper
 import org.springframework.amqp.rabbit.core.RabbitTemplate
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 import org.springframework.web.multipart.MultipartFile
 import java.io.File
+import java.io.IOException
+import java.nio.charset.StandardCharsets
 import java.util.*
 import java.util.logging.Logger
 
@@ -17,13 +17,15 @@ class RestHandler(
 ) {
     companion object {
         private val logger = Logger.getLogger(RestHandler::class.java.name)
+        private val resultDir = File("${System.getProperty("user.home")}/results")
     }
 
     private val home = System.getProperty("user.home")
-    private val uploadDir = File("$home/uploads")
+    private val uploadDir = File("$home/uploads/")
 
     @PostMapping("/upload")
-    private fun upload(@RequestParam("file") file: MultipartFile?): ResponseEntity<out Map<String, Any?>> {
+        private fun upload(@RequestParam("file") file: MultipartFile?): ResponseEntity<out Map<String, Any?>> {
+        logger.warning("Home is $home, uploadDir is $uploadDir")
         logger.warning("File received: $file, type is ${file?.contentType}")
 
         if (file == null || file.isEmpty) {
@@ -38,63 +40,69 @@ class RestHandler(
             return ResponseEntity(response, HttpStatus.BAD_REQUEST)
         }
 
-        val token = UUID.randomUUID().toString()
-        val destFile = File(uploadDir, token)
-        file.transferTo(destFile)
+        try {
+            val token = UUID.randomUUID().toString()
+            val destFile = File("$uploadDir/$token")
+            file.transferTo(destFile)
 
-        postMessage(token)
+            postMessage(token)
 
-        val response = mapOf(
-            "message" to "Processing file.",
-            "_links" to mapOf(
-                "self" to mapOf("href" to "/results/$token"),
-                "results" to mapOf("href" to "/results")
+            val response = mapOf(
+                "message" to "Processing file.",
+                "_links" to mapOf(
+                    "self" to mapOf("href" to "/results/$token"),
+                    "results" to mapOf("href" to "/results")
+                )
+
             )
-
-        )
-        return ResponseEntity(response, HttpStatus.ACCEPTED)
+            return ResponseEntity(response, HttpStatus.ACCEPTED)
+        } catch (e: IOException) {
+            logger.severe("Io error: $e")
+            return ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR)
+        }
     }
 
-//    @GetMapping("/results/{token}")
-//    fun results(@PathVariable token: String): ResponseEntity<Any> {
-//        val result = getResult(token)
-//
-//        return if (result.ready) {
-//            if (result.successful) {
-//                val transcr = readResult(token)
-//
-//                ResponseEntity.ok(
-//                    mapOf(
-//                        "ready" to true,
-//                        "successful" to true,
-//                        "result" to transcr,
-//                        "_links" to mapOf(
-//                            "self" to mapOf("href" to "/results/$token"),
-//                            "results" to mapOf("href" to "/results")
-//                        )
-//                    )
-//                )
-//            } else {
-//                ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-//                    .body(mapOf("status" to "ERROR", "error_message" to result.errorMessage))
-//            }
-//        } else {
-//            ResponseEntity.ok(
-//                mapOf(
-//                    "status" to "Running",
-//                    "_links" to mapOf(
-//                        "self" to mapOf("href" to "/results/$token"),
-//                        "results" to mapOf("href" to "/results")
-//                    )
-//                )
-//            )
-//        }
-//    }
+    @GetMapping("/results/{token}")
+    fun results(@PathVariable token: String): ResponseEntity<Any> {
+        val result = getResult(token)
+
+        return if (result != null && result.isFile) {
+            val transcription = result.inputStream().bufferedReader(StandardCharsets.UTF_8).use {
+                it.readText()
+            }
+
+            ResponseEntity.ok(
+                mapOf(
+                    "ready" to true,
+                    "successful" to true,
+                    "result" to transcription,
+                    "_links" to mapOf(
+                        "self" to mapOf("href" to "/results/$token"),
+                        "results" to mapOf("href" to "/results")
+                    )
+                )
+            )
+        } else {
+            ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(mapOf("status" to "ERROR", "error_message" to "Transcription result not found"))
+        }
+    }
 
     private fun postMessage(message: String) {
         logger.info("Posting message $message")
         rabbitTemplate.convertAndSend("WHISPER-QUEUE", message)
     }
 
+    private fun getResult(token: String): File? {
+        try {
+            resultDir.listFiles()?.forEach {
+                if (it.nameWithoutExtension == token) return it
+            }
+        } catch (e: IOException) {
+            logger.severe("IO err: $e")
+            return null
+        }
+        return null
+    }
 
 }
